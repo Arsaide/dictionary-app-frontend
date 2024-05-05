6f516fd8b34fcef9c111ea9dc252da9c057e3987
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-05-05 23:34:41 +0300

    - added enum colors constants

---
+++ src/libs/colors/ButtonColors.ts
@@ -0,0 +1,10 @@
+export enum ButtonColors {
+    WHITE = '#fff',
+    GRAY = '#b6b6b6',
+
+    DGREEN = '#5a8f29',
+    LGREEN = '#7dc738',
+
+    LRED = '#f54242',
+    DRED = '#c82333',
+}
---
+++ src/libs/colors/MainColors.ts
@@ -0,0 +1,15 @@
+export enum MainColors {
+    GRAY333 = '#333',
+    GRAY333_01 = 'rgba(255, 255, 255, 0.1)',
+    GRAY808 = '#808080',
+    GRAY595 = '#595959',
+    GRAY999 = '#999999',
+
+    WHITE = '#fff',
+
+    BLACK = '#000',
+    BLACK_06 = 'rgba(0, 0, 0, 0.6)',
+
+    RED = '#f54242',
+    GREEN = '#7dc738',
+}
