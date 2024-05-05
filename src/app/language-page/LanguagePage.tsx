92495f66ef77437825aaf35869b3e013bae2eaee
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-05-05 23:34:22 +0300

    - added pages

---
+++ src/app/language-page/LanguagePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LanguagePage = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default LanguagePage;
---
+++ src/app/language-page/lang-id/LangId.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LangId = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default LangId;
